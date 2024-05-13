8652e4ab7a857dcbed885375ea99224b372ea06d
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-13 08:40:54 +0200

    Stage 5/5

--- Virtual Piano/task/src/piano.js
+++ Virtual Piano/task/src/piano.js
@@ -1,42 +1,19 @@
-// class Piano {
-//     constructor(containerEl) {
-//         this.containerEl = containerEl;
-//
-//         document.addEventListener("keydown", this.handleKeyPressDown.bind(this));
-//     }
-//     async handleKeyPressDown(ev) {
-//         if (!"asdfghj".includes(ev.key)) {
-//             console.warn("Warning");
-//             return;
-//         }
-//         const key = ev.key.toUpperCase();
-//
-//         const audio = new Audio(`./sounds/${key}.mp3`);
-//         await audio.play();
-//         audio.remove();
-//     }
-// }
-//
-// const piano = new Piano(document.querySelector(".container"));
+class Piano {
+    constructor(containerEl) {
+        this.containerEl = containerEl;
 
-document.addEventListener("DOMContentLoaded", event => {
-    const keys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
-    const container = document.querySelector('.container');
-
-    for (let k of keys) {
-        let key = document.createElement('kbd');
-        key.textContent = k;
-        container.appendChild(key);
+        document.addEventListener("keydown", this.handleKeyPressDown.bind(this));
     }
+    async handleKeyPressDown(ev) {
+        if (!"adefghjstuwy".includes(ev.key)) {
+            return;
+        }
+        const key = ev.key.toUpperCase();
 
-    document.addEventListener("keypress", e => {
-        let char = e.code.charAt(3);
+        const audio = new Audio(`sounds/${key}.mp3`);
+        await audio.play();
+        audio.remove();
+    }
+}
 
-        if (keys.includes(char)) {
-            let audio = new Audio(`sounds/${char}.mp3`);
-            audio.play();
-        } else
-            console.log("Error: press a valid key!");
-    });
-});
-
+const piano = new Piano(document.querySelector(".container"));
